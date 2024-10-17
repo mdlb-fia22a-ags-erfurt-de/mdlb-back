fe6b090da37170ccd469cdbf914120605e33fc68
Baktyiar Kydyrberdi uulu <baktyiar.kydyrberdi@navimatix.de>
2024-10-17 10:20:34 +0200

    Initial Commit

---
+++ src/test/java/mdlb/fia22a/ags/de/mdlb_back/MdlbBackApplicationTests.java
@@ -0,0 +1,13 @@
+package mdlb.fia22a.ags.de.mdlb_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdlbBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
